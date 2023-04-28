0f77c2b5e1db91a52d0b4bf96e31922dba1ef841
Anh Bui <anhbhpd08263@fpt.edu.vn>
2023-04-28 21:52:44 +0700

    upload

---
+++ Tabs-UI/app.js
@@ -0,0 +1,28 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+const tabs = $$('.tab-item');
+const panes = $$('.tab-pane');
+
+const tabActive = $('.tab-item.active');
+const line = $('.tab .line');
+
+requestIdleCallback(function () {
+    line.style.left = tabActive.offsetLeft + "px";
+    line.style.width = tabActive.offsetWidth + "px";
+});
+
+
+tabs.forEach((tab, index) => {
+    const pane = panes[index];
+    tab.onclick = function () {
+        $('.tab-item.active').classList.remove('active');
+        $('.tab-pane.active').classList.remove('active');
+        
+        line.style.left = this.offsetLeft + 'px';
+        line.style.width = this.offsetWidth + 'px';
+
+        this.classList.add('active');
+        pane.classList.add('active');
+    }
+});
